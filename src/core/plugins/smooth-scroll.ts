fdf3437c547adf64f16630d232b62e839dfbd155
Saqib Syed <saqibsyed1014@gmail.com>
2023-10-30 23:37:40 +0500

    smooth scroll disabled on mobile

--- src/core/plugins/smooth-scroll.ts
+++ src/core/plugins/smooth-scroll.ts
@@ -2,14 +2,12 @@ import Lenis from "@studio-freight/lenis";
 
 const lenis = new Lenis({
     duration: 3,
-    touchMultiplier: 2,
     easing: (val) => Math.min(1, 1.001 - Math.pow(2, -10 * val)),
     smoothWheel: true,
     infinite: false,
-    syncTouch: true
 });
 
-function raf(time) {
+function raf(time :any) {
     lenis.raf(time);
     requestAnimationFrame(raf);
 }
