e99a290a1033e7d03f2c47e031616c723d01c23a
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-08-30 16:06:37 +0500

    minor fixes

--- src/core/utils/utils.functions.ts
+++ src/core/utils/utils.functions.ts
@@ -6,7 +6,7 @@ export const blogSharing = (socialPlatform :'linkedin' | 'facebook' | 'X') :void
             url = `https://www.linkedin.com/sharing/share-offsite/?url=${currentBlogURL}`;
             break;
         case "facebook":
-            url = `https://www.facebook.com/dialog/share?app_id=811715347841833&display=popup&href=${currentBlogURL}&redirect_uri=${currentBlogURL}`
+            url = `https://www.facebook.com/dialog/share?app_id=811715347841833&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F}&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer`
             break;
         case "X":
             url = `https://x.com/intent/post?text=Check out this blog!:&url=${currentBlogURL}%0A%0A&hashtags=DevDock,DevDock Blogs`;
