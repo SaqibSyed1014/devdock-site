464ac798b929d4aff38fb9b26720ebf846a336b2
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-09-02 13:44:39 +0500

    share links title added

--- src/pages/BlogDetails/components/ShareBlogLinks.vue
+++ src/pages/BlogDetails/components/ShareBlogLinks.vue
@@ -6,14 +6,17 @@ import { blogSharing } from "@/core/utils/utils.functions.ts";
 const shareLinks = [
   {
     icon: "i-mdi-facebook",
+    title: "Share on Facebook",
     onClick: () => blogSharing('facebook')
   },
   {
     icon: "i-mdi-twitter",
+    title: "Share on Twitter(X)",
     onClick: () => blogSharing('X')
   },
   {
     icon: "i-mdi-linkedin",
+    title: "Share on Linkedin",
     onClick: () => blogSharing('linkedin')
   }
 ]
@@ -34,6 +37,7 @@ function copyLink() {
     <template v-for="social in shareLinks">
       <div
           class="flex justify-center items-center w-10 h-10 bg-white border border-primary/50 rounded cursor-pointer group hover:bg-primary transition"
+          :title="social.title"
           @click="social.onClick"
       >
         <span
@@ -43,7 +47,7 @@ function copyLink() {
       </div>
     </template>
 
-    <div @click="copyLink" class="flex justify-center items-center w-10 h-10 bg-white border border-primary/50 rounded cursor-pointer group hover:bg-primary transition">
+    <div title="Copy URL" @click="copyLink" class="flex justify-center items-center w-10 h-10 bg-white border border-primary/50 rounded cursor-pointer group hover:bg-primary transition">
       <span class="text-xl xl:text-2xl text-primary i-mdi-link-variant group-hover:text-white transition"></span>
     </div>
   </div>
